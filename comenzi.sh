2967a2ffc994beb24c0b149552980aa194efb05e
Ionut Ojica <ionut.ojica@yahoo.com>
2024-09-19 18:56:20 +0200

    english translation

--- comenzi.sh
+++ comenzi.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
-# Actualizarea scriptului comenzi.php
+# Update of commands.php
 
-# Numele scriptului cu extensie si calea absoluta
+# Script name with extension and absolute path
 path_scriptName_ext=$(readlink -f "$0")
-# Calea absoluta a scriptului, terminata cu "/"
+# The absolute path of the script, terminated with "/"
 path_script=$(dirname "$path_scriptName_ext")/
-# Numele scriptului cu extensie, dar fara cale
+# Script name with extension, but no path
 scriptName_ext=$(basename "$path_scriptName_ext")
-# Numele scriptului fara extensie
+# Script name without extension
 scriptName="${scriptName_ext%.sh}"
 
-# Verifica daca este solicitata operatiunea de undo
+# Check if the undo operation is requested
 case "$1" in
   undo|revert|restaureaza|reface)
-    echo "$(date +%H:%M:%S) Se efectueaza undo..."
+    echo "$(date +%H:%M:%S) Undo is performed..."
 
-    # Verifica si inlocuieste fisierul .sh daca exista backup
+    # Check and replace the .sh file if there is a backup
     if [ -f "${path_scriptName_ext}.bak" ]; then
       mv "${path_scriptName_ext}.bak" "${path_scriptName_ext}"
-      echo "Scriptul bash a fost restaurat la versiunea anterioara."
+      echo "The bash script has been restored to the previous version."
     else
-      echo "Nu exista backup pentru scriptul bash."
+      echo "There is no backup for the bash script."
     fi
 
-    # Verifica si inlocuieste fisierul .php daca exista backup
+    # Check and replace the .php file if there is a backup
     if [ -f "${path_script}${scriptName}.php.bak" ]; then
       mv "${path_script}${scriptName}.php.bak" "${path_script}${scriptName}.php"
-      echo "Scriptul PHP a fost restaurat la versiunea anterioara."
+      echo "The PHP script has been restored to the previous version."
     else
-      echo "Nu exista backup pentru scriptul PHP."
+      echo "There is no backup for the PHP script."
     fi
   ;;
   *)
-    # Continua cu logica de actualizare daca nu este cerut undo
+    # Continue with update logic if no undo is requested
 
-    echo "$(date +%H:%M:%S) Descarc versiunile actuale"
+    echo "$(date +%H:%M:%S) Download current versions"
     wget "https://raw.githubusercontent.com/IonutOjicaDE/mautic-commands/main/$scriptName_ext?$(date +%s)" -O "${path_scriptName_ext}.new"
     wget "https://raw.githubusercontent.com/IonutOjicaDE/mautic-commands/main/${scriptName}.php?$(date +%s)" -O "${path_script}${scriptName}.php.new"
 
-    # Verifica daca fisierul .sh este diferit si actualizeaza
+    # Check if the .sh file is different and update it
     if ! cmp -s "${path_scriptName_ext}.new" "${path_scriptName_ext}"; then
       mv "${path_scriptName_ext}" "${path_scriptName_ext}.bak"
       mv "${path_scriptName_ext}.new" "${path_scriptName_ext}"
-      echo "Versiunea noua de script bash a fost implementata."
+      echo "The new version of the bash script has been implemented."
     else
-      echo "Ai deja ultima versiune de script bash implementata."
+      echo "You already have the latest version of the bash script implemented."
     fi
 
-    # Verifica daca fisierul .php este diferit si actualizeaza
+    # Check if the .php file is different and update it
     if ! cmp -s "${path_script}${scriptName}.php.new" "${path_script}${scriptName}.php"; then
       mv "${path_script}${scriptName}.php" "${path_script}${scriptName}.php.bak"
       mv "${path_script}${scriptName}.php.new" "${path_script}${scriptName}.php"
-      echo "Versiunea noua de script php a fost implementata."
+      echo "The new version of the php script has been implemented."
     else
-      echo "Ai deja ultima versiune de script php implementata."
+      echo "You already have the latest version of the php script implemented."
     fi
   ;;
 esac
